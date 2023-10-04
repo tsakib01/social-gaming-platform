8488677c48caf4891ecd95d7ca88b0826add1d23
o0victor0o <victoroltean09@gmail.com>
2023-10-04 14:10:18 -0700

    Created a temporary main target executable.
    The executable accepts a game file path argument.
    If provided, it will run printTestConfig(), defined in gameConfigLoader lib.

---
+++ tools/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string_view>
+#include "GameConfigLoader.h"
+
+int main(int argc, char** argv) {
+    if(argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " [game file input]\n";
+        return EXIT_FAILURE;
+    }
+
+    std::string_view gameFilePath = argv[1];
+    std::cout << "Game file path: " << gameFilePath << "\n";
+
+    printTestConfig();
+
+    return EXIT_SUCCESS;
+}
