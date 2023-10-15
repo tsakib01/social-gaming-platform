084301eca6d85c60e4f7ee1542ab04c9363b2bc2
memora <michael_mora@sfu.ca>
2023-10-15 02:00:36 -0700

    Changed from unordered_maps to maps, changed setters and maps in UserManager to use the Value type.

--- lib/game-rule-engine/include/UserManager.h
+++ lib/game-rule-engine/include/UserManager.h
@@ -2,10 +2,14 @@
 #define USER_MANAGER_H
 #include "Player.h"
 #include "Spectator.h"
-#include <unordered_map> 
+#include <map> 
 #include <string>
 #include <vector>
 #include <variant>
+using Primitive = std::variant<int, bool, std::string_view>;
+using Map = std::map<std::string_view, Primitive>;
+using List = std::vector<std::variant<Primitive, Map>>;
+using Value = std::variant<Primitive, Map, List>;
 
 class UserManager {
     public:
@@ -16,20 +20,22 @@ class UserManager {
 
     std::vector<Player> getPlayers();
     std::vector<Spectator> getSpectators();
+    
     void setPlayers(std::vector<Player> players);
     void setSpectators(std::vector<Spectator> spectators);
+    void setPlayerValues(Player player, std::vector<Value>values);
+    void setSpectatorValue(Spectator spectator, std::vector<Value> values);
 
     //TODO: Figure out how to call getters and setters
 
     private:
     ~UserManager();
+
     std::vector<Player> players;
-    // Keys for for players TENTATIVELY maps from their name to a variant that supports
-    // an "empty" variant (aka monostate), int, bool and string.
-    std::unordered_map<std::string, std::vector<std::variant<std::monostate, int, bool, std::string>>> per_player; 
+    std::map<std::string, std::vector<Value>> per_player; 
     std::vector<Spectator> spectators;
     // Spectators don't have unique names, so we can just use their user IDs which are of type int
-    std::unordered_map<int, std::vector<std::variant<std::monostate, int, bool, std::string>>>per_spectator;
+    std::map<int, std::vector<Value>>per_spectator;
 };
 
 #endif
--- lib/game-rule-engine/src/UserManager.cpp
+++ lib/game-rule-engine/src/UserManager.cpp
@@ -11,9 +11,16 @@ void UserManager::setPlayers(std::vector<Player> playerList){
     }
 }
 
+// Update whole set of player's assignment values; is there a way for us to just update
+// a smaller, select number of values instead? Maybe here or elsewhere?
+void UserManager::setPlayerValues(Player player, std::vector<Value> values){
+    per_player[player.getName()] = values;
+}
+
 std::vector<Spectator> UserManager::getSpectators(){
     return spectators;
 }
+
 
 // Initial setup of maps for spectators
 void UserManager::setSpectators(std::vector<Spectator> spectatorList){
@@ -22,4 +29,7 @@ void UserManager::setSpectators(std::vector<Spectator> spectatorList){
     }
 }
 
+void UserManager::setSpectatorValue(Spectator spectator, std::vector<Value> value){ 
+    per_spectator[spectator.getUserID()] = value;
+}
 //TODO: create functions that will update the maps from setPlayers and setSpectators
