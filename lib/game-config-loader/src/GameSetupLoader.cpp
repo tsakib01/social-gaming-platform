86142ca77761f459204f64d628eb579df79b3323
jiarongt <jiarong_tang@sfu.ca>
2023-10-17 10:00:16 -0700

    finished parsing setup

--- lib/game-config-loader/src/GameSetupLoader.cpp
+++ lib/game-config-loader/src/GameSetupLoader.cpp
@@ -3,16 +3,35 @@
 Setup::Setup(KIND kind,std::string_view prompt):kind(kind),prompt(prompt),restInfo(std::string_view{}){}
 
 Setup::Setup(KIND kind,std::string_view prompt,std::string_view restInfo):kind(kind),prompt(prompt),restInfo(restInfo){}
+KIND convertToKind(std::string_view kind){
+    std::string temp{kind.begin(),kind.end()};
+    if(temp=="integer"){
+        return KIND::INTEGER;
+    }
+    else if(temp=="string"){
+        return KIND::STRING;
+    }
+    else if(temp=="boolean"){
+        return KIND::BOOLEAN;
+    }
+    else if(temp=="enum"){
+        return KIND::ENUM;
+    }
+    else {
+        std::runtime_error("kind not support");
+    }
 
+}
 constexpr const char* KINDToString(KIND kind) noexcept
 {
-switch (kind)
-{
-case KIND::INTEGER: return "integer";
-case KIND::STRING: return "string";
-case KIND::BOOLEAN: return "boolean";
-case KIND::ENUM: return "enum";
-}
+    switch (kind)
+    {
+    case KIND::INTEGER: return "integer";
+    case KIND::STRING: return "string";
+    case KIND::BOOLEAN: return "boolean";
+    case KIND::ENUM: return "enum";
+    }
+    std::runtime_error("kind not support yet");
 }
 void Setup::intProcess(){
     this->print();
@@ -40,13 +59,34 @@ void Setup::print(){
     std::cout<<prompt<<std::endl;
     std::cout<<restInfo<<std::endl;
 }
-GameSetupLoader::GameSetupLoader(std::string_view source){
+GameSetupLoader::GameSetupLoader(std::string_view source):source(source){}
 
-}
 Setup GameSetupLoader::convertNodetoSetup(const ts::Node& node){
+    int childCount =node.getNumChildren();
+    if(childCount<7){
+        std::runtime_error("no enough information in setup");
+    }
+    KIND kind=convertToKind(node.getChild(3).getType());
+    std::string_view prompt=node.getChild(5).getSourceRange(source);
+    if(childCount>7){
+
+        ts::Extent first=node.getChild(6).getByteRange();
+
+        ts::Extent last=node.getChild(childCount-2).getByteRange();
+        std::string_view restInfo=source.substr(first.start,last.end-first.start);
+        return {kind,prompt,restInfo};
+    }
+    return {kind,prompt};
 
 }
 std::unique_ptr<std::vector<Setup>> GameSetupLoader::getGameSetup (const ts::Node& node){
+    int numChildCount=node.getNumNamedChildren();
 
+    std::unique_ptr<std::vector<Setup>> setups=std::make_unique<std::vector<Setup>>();
+
+    for(int index=3;index<numChildCount;index++){
+        setups->push_back(convertNodetoSetup(node.getNamedChild(index)));
+    }
+    return setups;
 }
 
--- tools/main.cpp
+++ tools/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cpp-tree-sitter.h>
 #include "GameConfigLoader.h"
+#include "GameSetup.h"
+#include "GameSetupLoader.h"
 #include "GameInstance.h"
 
 int main(int argc, char** argv) {
@@ -16,64 +18,10 @@ int main(int argc, char** argv) {
     GameConfigLoader gameConfigLoader{gameFilePath};
     auto rules = gameConfigLoader.createGameRules();
     auto state = gameConfigLoader.createGameState();
-    
+    auto setup = gameConfigLoader.createGameSetup();
+    setup->processGameSetups();
     GameInstance game = GameInstance(rules, std::move(state));
 
-    //get the game file
-//    ts::Language language = tree_sitter_socialgaming();
-//    ts::Parser parser{language};
-//
-//    std::ifstream ifs(gameFilePath);
-//    std::stringstream buffer;
-//    buffer << ifs.rdbuf();
-//    std::string sourcecode = buffer.str();
-//    ifs.close();
-//    std::cout << sourcecode << '\n';
-//    ts::Tree tree = parser.parseString(sourcecode);
-//    ts::Node root = tree.getRootNode();
-//
-//
-//    // get configuration node
-//    ts::Node configuration = root.getChildByFieldName("configuration");
-//    std::cout<<configuration.getType();
-//    int configCount=configuration.getNumNamedChildren();
-//    std::cout<<"config"<<std::endl;
-//    std::cout << configuration.getType() <<" | Num Named children:"<<configCount<<'\n'<< " | Num children: " << configuration.getNumChildren() << '\n'<<std::endl;
-//    ts::Extent configRange = configuration.getByteRange();
-//     std::cout<<"configuration:"<<std::endl;
-//     std::cout << sourcecode.substr(configRange.start, configRange.end - configRange.start) << '\n'<<std::endl;
-//
-//
-//    //perser the setup into a vector
-//    std::vector<std::pair<std::string, std::vector<std::pair<std::string,std::string>>>> setups;
-//    for(int index=3;index<configCount;index++){
-//        ts::Extent identifierRange=configuration.getNamedChild(index).getChild(0).getByteRange();
-//        // identifier is the name of each setup;
-//        std::string identifier=sourcecode.substr(identifierRange.start,identifierRange.end-identifierRange.start);
-//        //get the num of named children
-//        int childCount =configuration.getNamedChild(index).getNumChildren();
-//        std::vector<std::pair<std::string,std::string>> values;
-//        for(int j=2;j<childCount-1;j+=2){
-//            ts::Extent leftRange=configuration.getNamedChild(index).getChild(j).getByteRange();
-//            ts::Extent rightRange=configuration.getNamedChild(index).getChild(j+1).getByteRange();
-//
-//           std::string left=sourcecode.substr(leftRange.start,leftRange.end-leftRange.start);
-//            std::string right=sourcecode.substr(rightRange.start,rightRange.end-rightRange.start);
-//
-//            values.push_back(std::make_pair(left,right));
-//        }
-//        setups.push_back(std::make_pair(identifier,values));
-//
-//    }
-//
-//    //print the setups
-//    for( auto elem : setups){
-//        std::cout<<elem.first<<std::endl;
-//        for(auto& ele:elem.second){
-//            std::cout<<"   "<<ele.first<<" "<<ele.second<<std::endl;
-//        }
-//        std::cout<<std::endl;
-//    }
     return EXIT_SUCCESS;  
 
 }
