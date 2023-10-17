e537c9dc639b027c9bffe303526e695f315c1158
jiarongt <jiarong_tang@sfu.ca>
2023-10-16 20:42:17 -0700

    add setuploader.h

---
+++ lib/game-config-loader/include/GameSetupLoader.h
@@ -0,0 +1,54 @@
+#include<string_view>
+#include<vector>
+#include <memory>
+#include <cpp-tree-sitter.h>
+enum KIND{
+    INTEGER, STRING, BOOLEAN, ENUM,
+};
+class GameSetupLoader{
+public:
+    GameSetupLoader(std::string_view source);
+    Setup convertNodetoSetup(const ts::Node& node);
+    std::unique_ptr<std::vector<Setup>>(const ts::Node& node);
+private:
+    std::string_view source;
+
+};
+class Setup{
+public:
+    Setup(KIND kind,std::string_view prompt);
+    Setup(KIND kind,std::string_view prompt,);
+    void intProcess();
+    void boolProcess();
+    void strProcess();
+    void processSetup();
+private:
+    std::string_view identifier;
+    KIND kind;
+    std::string_view prompt;
+    //may change to other type;
+    std::string_view restInfo;
+};
+//class intSetup:public  Setup{
+//public:
+//    void processSetup();
+//    intSetup(ts::N);
+//private:
+//    std::string_view name;
+//    std::string_view prompt;
+//};
+//class boolSetup:public  Setup{
+//public:
+//    void processSetup();
+//private:
+//    std::string_view name;
+//    std::string_view prompt;
+//
+//};
+//class strSetup:public  Setup{
+//public:
+//    void processSetup();
+//private:
+//    std::string_view name;
+//    std::string_view prompt;
+//};
