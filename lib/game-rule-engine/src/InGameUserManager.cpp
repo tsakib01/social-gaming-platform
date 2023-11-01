76f1888487127c19dc2e9a4c3a2a5e1c3d0089e0
memora <michael_mora@sfu.ca>
2023-10-31 22:32:57 -0700

    Removed/changed comments about using a map (we are using pairs instead for <Role, Environment>

--- lib/game-rule-engine/src/InGameUserManager.cpp
+++ lib/game-rule-engine/src/InGameUserManager.cpp
@@ -3,8 +3,6 @@
 
     // Assume the role is passed as an Enum (stored within UserManager).
     void InGameUserManager::addNewUser(uint32_t userID, Role role, Environment userStates){
-        // Have to make a new map which will be inserted as the value.
-        // Might it be possible for Role and Environment to be passed in the arguments as a map?
         std::pair<Role, Environment> userStartingStates;
         userStartingStates.first = role;
         userStartingStates.second = userStates;
@@ -27,7 +25,7 @@
     }
     
     // Recall that m_userStates is of type std::map<uint32_t, std::map<Role, Environment>>
-    // Create a new map to replace the existing one that the User ID maps to.
+    // Create a new pair to replace the existing one that the User ID maps to.
     void InGameUserManager::setStatesOfUser(uint32_t userID, Role role, Environment states){
         std::pair<Role, Environment> statesToSet;
         statesToSet.first = role;
