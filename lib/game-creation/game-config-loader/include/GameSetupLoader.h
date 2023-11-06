cd018411614c90555c4db407d25ad5ad58c17e57
jiarongt <jiarong_tang@sfu.ca>
2023-11-06 03:35:35 -0800

    finih store the setup into virant type and check the response from the user

--- lib/game-creation/game-config-loader/include/GameSetupLoader.h
+++ lib/game-creation/game-config-loader/include/GameSetupLoader.h
@@ -4,17 +4,26 @@
 #include<string_view>
 #include<vector>
 #include <memory>
+#include <variant>
+#include <map>
 #include <cpp-tree-sitter.h>
 enum KIND{
     INTEGER, STRING, BOOLEAN, ENUM,
 };
+using Range = std::pair<int,int>;
+using ChoiceList = std::map<std::string_view,std::string_view>;
+using Domain = std::variant<Range, ChoiceList> ;
+
 class Setup{
 public:
     Setup(std::string_view identifier,KIND kind,std::string_view prompt);
-    Setup(std::string_view identifier,KIND kind,std::string_view prompt,std::string_view restInfo);
+    Setup(std::string_view identifier,KIND kind,std::string_view prompt,std::string_view restInfo, Domain domain);
+
+    bool checkResponse(std::string_view response);
     void intProcess();
     void boolProcess();
     void strProcess();
+    void enumProcess();
     void processSetup();
     void print();
 private:
@@ -23,7 +32,22 @@ private:
     std::string_view prompt;
     //may change to other type;
     std::string_view restInfo;
+    Domain domain;
+
 };
+struct DomainPrinter {
+    void operator()(const Range& range) const ;
+
+    void operator()(const ChoiceList& choices) const ;
+};
+
+//for check the response from thr user
+struct DomainChecker{
+    bool operator()(const Range& range, std::string_view input ) const ;
+
+    bool operator()(const ChoiceList& choices, std::string_view input) const ;
+};
+
 class GameSetupLoader{
 public:
     GameSetupLoader(std::string_view source);
@@ -34,27 +58,5 @@ private:
 
 };
 
-//class intSetup:public  Setup{
-//public:
-//    void processSetup();
-//    intSetup(ts::N);
-//private:
-//    std::string_view name;
-//    std::string_view prompt;
-//};
-//class boolSetup:public  Setup{
-//public:
-//    void processSetup();
-//private:
-//    std::string_view name;
-//    std::string_view prompt;
-//
-//};
-//class strSetup:public  Setup{
-//public:
-//    void processSetup();
-//private:
-//    std::string_view name;
-//    std::string_view prompt;
-//};
+
 #endif
--- lib/game-creation/game-config-loader/src/GameSetupLoader.cpp
+++ lib/game-creation/game-config-loader/src/GameSetupLoader.cpp
@@ -1,8 +1,9 @@
 #include "GameSetupLoader.h"
 #include <iostream>
+#include <charconv>
 Setup::Setup(std::string_view identifier,KIND kind,std::string_view prompt):identifier(identifier),kind(kind),prompt(prompt),restInfo(std::string_view{}){}
 
-Setup::Setup(std::string_view identifier,KIND kind,std::string_view prompt,std::string_view restInfo):identifier(identifier),kind(kind),prompt(prompt),restInfo(restInfo){}
+Setup::Setup(std::string_view identifier,KIND kind,std::string_view prompt,std::string_view restInfo, Domain domain):identifier(identifier),kind(kind),prompt(prompt),restInfo(restInfo), domain(domain){}
 KIND convertToKIND(std::string_view kind){
     if(kind!="integer"&&kind!="string"&&kind!="boolean"&&kind!="enum"){
         throw std::runtime_error("kind not support");
@@ -20,6 +21,7 @@ KIND convertToKIND(std::string_view kind){
         return KIND::ENUM;
     }
 }
+
 constexpr std::string_view KINDToString(KIND kind)
 {
     if(kind!=KIND::INTEGER&&kind!=KIND::STRING&&kind!=KIND::BOOLEAN&&kind!=KIND::ENUM){
@@ -34,6 +36,8 @@ constexpr std::string_view KINDToString(KIND kind)
     }
 
 }
+
+
 void Setup::intProcess(){
     this->print();
 }
@@ -43,15 +47,21 @@ void Setup::boolProcess(){
 void Setup::strProcess(){
     this->print();
 }
+void Setup::enumProcess(){
+    this->print();
+}
 void Setup::processSetup(){
-    if(kind==KIND::INTEGER){
+    if(kind == KIND::INTEGER){
         this->intProcess();
     }
-    if(kind==KIND::STRING){
+    if(kind == KIND::STRING){
         this->strProcess();
     }
-    if(kind==KIND::BOOLEAN){
+    if(kind == KIND::BOOLEAN){
         this->boolProcess();
+    }
+    if(kind == KIND::ENUM){
+        this-> enumProcess();
     }
 }
 void Setup::print(){
@@ -59,9 +69,166 @@ void Setup::print(){
     std::cout<<KINDToString(kind)<<std::endl;
     std::cout<<prompt<<std::endl;
     std::cout<<restInfo<<std::endl;
+    if(kind==KIND::INTEGER||kind==KIND::ENUM){
+        std::visit(DomainPrinter{}, domain);
+    }
+
     std::cout<<std::endl;
 }
+bool Setup::checkResponse(std::string_view response){
+    switch (kind)
+    {
+        case KIND::INTEGER:
+            return std::visit([&response](const auto& arg) {
+                return DomainChecker{}(arg, response);
+            }, domain);
+
+        case KIND::STRING: return true;
+        case KIND::BOOLEAN:
+            if(response=="y"||response=="n"){
+                return true;
+            }
+            else{
+                return false;
+            }
+        case KIND::ENUM: return std::visit([&response](const auto& arg) {
+                return DomainChecker{}(arg, response);
+            }, domain);
+    }
+}
+
+//DomainPrinter
+void DomainPrinter::operator()(const Range& range) const {
+    std::cout << "Range: (" << range.first << ", " << range.second << ")\n";
+}
+
+void DomainPrinter::operator()(const ChoiceList& choices) const {
+    std::cout << "ChoiceList:\n";
+    for (const auto& [key, value] : choices) {
+        std::cout << "  " << key << ": " << value << "\n";
+    }
+}
+//DomainChecker
+bool DomainChecker::operator()(const Range& range, std::string_view input) const {
+    int num;
+    auto[numPtr, numEc] = std::from_chars(input.data(), input.data() + input.size(), num);
+    if (numEc != std::errc()) {
+        return false;
+    }
+    if(num >= range.first && num <= range.second){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+bool DomainChecker::operator()(const ChoiceList& choices, std::string_view input) const {
+    if(choices.find(input)==choices.end()){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
 GameSetupLoader::GameSetupLoader(std::string_view source):source(source){}
+
+Range convertToRange(std::string_view restInfo){
+    auto openParen = restInfo.find('(');
+    auto closeParen = restInfo.find(')');
+
+    // If either parenthesis is not found, or they are in the wrong order, throw an error
+    if (openParen == std::string_view::npos || closeParen == std::string_view::npos || closeParen < openParen) {
+        throw std::invalid_argument("Invalid input string");
+    }
+
+    // Extract the numbers as a substring
+    std::string_view numbers = restInfo.substr(openParen + 1, closeParen - openParen - 1);
+
+    // Trim the spaces around the comma to properly find its position
+    auto comma = numbers.find(',');
+    if (comma == std::string_view::npos) {
+        throw std::invalid_argument("Invalid range format");
+    }
+
+    // Trim spaces from the first number
+    std::string_view firstNum = numbers.substr(0, comma);
+    firstNum.remove_prefix(std::min(firstNum.find_first_not_of(' '), firstNum.size()));
+
+    // Trim spaces from the second number
+    std::string_view secondNum = numbers.substr(comma + 1);
+    secondNum.remove_prefix(std::min(secondNum.find_first_not_of(' '), secondNum.size()));
+
+    // Parse the first number
+    int first, second;
+    auto[firstPtr, firstEc] = std::from_chars(firstNum.data(), firstNum.data() + firstNum.size(), first);
+    if (firstEc != std::errc()) {
+        throw std::invalid_argument("Invalid first number");
+    }
+
+    // Parse the second number
+    auto[secondPtr, secondEc] = std::from_chars(secondNum.data(), secondNum.data() + secondNum.size(), second);
+    if (secondEc != std::errc()) {
+        throw std::invalid_argument("Invalid second number");
+    }
+
+    // Return the pair of numbers
+    return {first, second};
+}
+
+ChoiceList convertToChoiceList( std::string_view restInfo){
+    ChoiceList choices;
+    while (!restInfo.empty()) {
+        // Find the start of the key
+        std::size_t keyStart = restInfo.find('\'');
+        if (keyStart == std::string_view::npos) break; // No more keys
+        restInfo.remove_prefix(keyStart + 1); // Skip past the starting quote
+
+        // Find the end of the key
+        std::size_t keyEnd = restInfo.find('\'');
+        if (keyEnd == std::string_view::npos) throw std::runtime_error("Mismatched quotes in key");
+        std::string_view key = restInfo.substr(0, keyEnd);
+        restInfo.remove_prefix(keyEnd + 1); // Skip past the ending quote
+
+        // Find the start of the value
+        std::size_t valueStart = restInfo.find('\'');
+        if (valueStart == std::string_view::npos) throw std::runtime_error("No starting quote for value");
+        restInfo.remove_prefix(valueStart + 1); // Skip past the starting quote
+
+        // Find the end of the value
+        std::size_t valueEnd = restInfo.find('\'');
+        if (valueEnd == std::string_view::npos) throw std::runtime_error("Mismatched quotes in value");
+        std::string_view value = restInfo.substr(0, valueEnd);
+        restInfo.remove_prefix(valueEnd + 1); // Skip past the ending quote
+
+        // Add the parsed key-value pair to the map
+        choices.emplace(key, value);
+
+        // Skip any trailing commas or whitespace before the next key
+        std::size_t nextKeyStart = restInfo.find_first_not_of(", \t\n\r");
+        if (nextKeyStart != std::string_view::npos) {
+            restInfo.remove_prefix(nextKeyStart);
+        }
+    }
+    return choices;
+}
+
+
+Domain convertStringToDomain(const KIND kind, const std::string_view restInfo){
+
+    if(kind!=KIND::INTEGER&&kind!=KIND::ENUM){
+        throw std::runtime_error("kind not support yet");
+    }
+    Domain toReturn;
+    if(kind==KIND::INTEGER){
+        toReturn = convertToRange(restInfo);
+    }
+    if(kind==KIND::ENUM){
+        toReturn = convertToChoiceList(restInfo);
+    }
+    return toReturn;
+}
+
 
 std::unique_ptr<Setup> GameSetupLoader::convertNodetoSetup(const ts::Node& node){
     if(node.getNumChildren()<7){
@@ -78,7 +245,7 @@ std::unique_ptr<Setup> GameSetupLoader::convertNodetoSetup(const ts::Node& node)
 
         ts::Extent last=node.getChild(childCount-2).getByteRange();
         std::string_view restInfo=source.substr(first.start,last.end-first.start);
-        return std::make_unique<Setup>(Setup{identifier,kind,prompt,restInfo});
+        return std::make_unique<Setup>(Setup{identifier,kind,prompt,restInfo,convertStringToDomain(kind,restInfo)});
     }
     return std::make_unique<Setup>(Setup{identifier,kind,prompt});
 
