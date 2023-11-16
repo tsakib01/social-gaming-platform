a3307e740701f6e5e0312cf4f446be1d794694d4
memora <michael_mora@sfu.ca>
2023-11-16 05:53:43 -0800

    Adjusted last test further to doublecheck that testReturnedStates doesn't match nullEnvironment

--- test/inGameUserManagerTests.cpp
+++ test/inGameUserManagerTests.cpp
@@ -49,10 +49,11 @@ TEST(InGameUserManagerTests, CanGetStatesOfUser){
 
     GameEnvironment::Environment nonNullDummyEnvironment = std::move(testMap);
     inGameUManager.addNewUser(dummyUserID, dummyRole, std::move(nonNullDummyEnvironment));
-    GameEnvironment::Environment testReturnedStates = std::move(inGameUManager.getStatesOfUser(dummyUserID));
+    GameEnvironment::Environment testReturnedStates = inGameUManager.getStatesOfUser(dummyUserID);
     ASSERT_TRUE(std::move(testReturnedStates).find(testIdentifier) != testReturnedStates.end());
 }
 
+// Test overwriting an Environment with nothing in it, with an Environment that does have something in it.
 TEST(InGameUserManagerTests, CanSetStatesOfExistingUser){
     InGameUserManager inGameUManager;
     uint32_t dummyUserID = 1;
@@ -60,21 +61,31 @@ TEST(InGameUserManagerTests, CanSetStatesOfExistingUser){
     
     // We care about using the Identifier so that we can find the user's Value.
     GameEnvironment::Identifier testIdentifier = "testidentifier";
-    
-    // Test overwriting an Environment with nothing in it.
+
     GameEnvironment::Environment nullEnvironment;
 
-    std::unique_ptr<GameEnvironment::Value> testValue;
+    // Load Value with an int which nonNullDummyEnvironment will get (and nullEnvironment won't)
+    GameEnvironment::Value testValue;
+    testValue.value = 500;
+    std::unique_ptr<GameEnvironment::Value> testValuePtr;
+    testValuePtr -> value;
+    
     std::map<GameEnvironment::Identifier, std::unique_ptr<GameEnvironment::Value>> testMap;
-    testMap.insert({testIdentifier, std::move(testValue)});
+    testMap.insert({testIdentifier, std::move(testValuePtr)});
+
+    // Begins with nullEnvironment which doesn't have any elements in its map.
     GameEnvironment::Environment nonNullDummyEnvironment = std::move(testMap);
     inGameUManager.addNewUser(dummyUserID, dummyRole, std::move(nullEnvironment));
-    
-    // Without using the nonNullDummyEnvironment, the assert would fail.
+
     inGameUManager.setStatesOfUser(dummyUserID, dummyRole, std::move(nonNullDummyEnvironment));
-    GameEnvironment::Environment testReturnedStates = std::move(inGameUManager.getStatesOfUser(dummyUserID));
+    GameEnvironment::Environment testReturnedStates = inGameUManager.getStatesOfUser(dummyUserID);
+
+    // These prove that the testIdentifier can be found in testReturnedStates but not nullEnvironmentStates
+    // Without using the setStatesOfUser function and therefore nonNullDummyEnvironment, the next assert would fail.
     ASSERT_TRUE(testReturnedStates.find(testIdentifier) != testReturnedStates.end());
-    
-    // Check that the identifier exists in both and is mapped to the same value.
-    ASSERT_EQ(testReturnedStates[testIdentifier], nonNullDummyEnvironment[testIdentifier]);
+    ASSERT_TRUE(nullEnvironment.find(testIdentifier) == nullEnvironment.end());
+
+    // Finally, this test proves that testResturnedStates's first element
+    // (testIdentifier) is not equivalent to nullEnvironment's first element (which has nothing).
+    ASSERT_TRUE(testReturnedStates.begin() != nullEnvironment.begin());
 }
