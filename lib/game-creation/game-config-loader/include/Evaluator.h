7bcd95dc22994ec342ae009b4466a121476a3fa4
tsakib <tsakib@sfu.ca>
2023-11-30 00:02:58 -0800

    adds new interface for list modifying operations

--- lib/game-creation/game-config-loader/include/Evaluator.h
+++ lib/game-creation/game-config-loader/include/Evaluator.h
@@ -11,6 +11,11 @@ enum class OPERATOR {
     ADD, SUBTRACT, MULTIPLY, DIVIDE, OR, AND, NOT, EQUAL
 };
 
+// Allowed operations for modifying lists by evaluator
+enum class LISTMODIFIER {
+    EXTEND, REVERSE, SHUFFLE
+};
+
 // An interface of Operation
 class Operation {
 public:
@@ -31,17 +36,41 @@ private:
     virtual bool getSpecificationImpl(std::vector<const GameEnvironment::Value*> values) const = 0;
 };
 
+// An interface of Operation
+class ListModifyOperation {
+public:
+    // Evaluates given arguments to GameEnvironment::Value
+    void evaluate(std::vector<GameEnvironment::Value*> values) {
+        // Operation specification not meet
+        if (!getSpecification(values)){
+            throw std::runtime_error("The operation specification is not met");
+        }
+        evaluateImpl(values);
+    }
+    // Check given arguments meets specification of Operation
+    bool getSpecification(std::vector<GameEnvironment::Value*> values) const {
+        return getSpecificationImpl(values);
+    }
+private:
+    virtual void evaluateImpl(std::vector<GameEnvironment::Value*> values) = 0;
+    virtual bool getSpecificationImpl(std::vector<GameEnvironment::Value*> values) const = 0;
+};
+
 class Evaluator{
 public:
     // Evaluate given arguments with given operator
     GameEnvironment::Value evaluate(OPERATOR operationEnum, std::vector<const GameEnvironment::Value*> values);
+    // Modify lists based on given action
+    void evaluate(LISTMODIFIER listModifierEnum, std::vector<GameEnvironment::Value*> values);
     // Add an associate from operator to operation
     void registerOperation(OPERATOR operatorEnum, std::unique_ptr<Operation> operation);
+    void registerOperation(LISTMODIFIER listModifierEnum, std::unique_ptr<ListModifyOperation> listModifyOperation);
     // Default factory method
     static Evaluator defaultEvaluatorFactory();
 private:
     // Map that can associate from operator to operation 
     std::map<OPERATOR, std::unique_ptr<Operation>> operatorToOperation; 
+    std::map<LISTMODIFIER, std::unique_ptr<ListModifyOperation>> listModifierToListModifyOperation;
 };
 
 #endif
