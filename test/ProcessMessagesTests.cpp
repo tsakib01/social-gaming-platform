1d7d4671299e6c4f7c43b9ed6cb9d6ce8ea68a6f
bda37 <bda37@sfu.ca>
2023-12-03 23:32:33 -0800

    Add test files for ProcessMessages and ServerManager

---
+++ test/ProcessMessagesTests.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+#include "ServerManager.h"
+
+// basic build response test
+
+// process new test
+
+// process intro test
+
+// process join game test
+
+// process game select test
+
+// process game config tests
+
+// process game wait test
+
+// process game runnign test
+
+// build group message test
+
+// build game files test
+
+// get game fiels test
---
+++ test/ServerManagerTests.cpp
@@ -0,0 +1,5 @@
+// test for build user messages
+#include <gtest/gtest.h>
+#include "ServerManager.h"
+
+
