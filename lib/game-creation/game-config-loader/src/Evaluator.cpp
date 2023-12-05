80cba89b1a310ccc67d69054c8d6498e5e566792
nka73 <nka73@sfu.ca>
2023-12-05 13:00:25 -0800

    Fix a bug and remove COLLECT operation.

--- lib/game-creation/game-config-loader/src/Evaluator.cpp
+++ lib/game-creation/game-config-loader/src/Evaluator.cpp
@@ -338,18 +338,14 @@ private:
 // TODO -> Support Map
 class ContainOperation final : public Operation {
 public:
-    ContainOperation(Evaluator& evaluator)
-    : evaluator(evaluator)
-    {}
 private:
     struct ContainOperationVisitor {
         GameEnvironment::Value operator()(const std::unique_ptr<GameEnvironment::List>& list){
-            auto result = GameEnvironment::Value(true);
-            
+            auto result = GameEnvironment::Value(false);
             // Check any of the element in the list is equal to target
             for (const auto& value : *list){
-                auto isEqual = evaluator.evaluate(OPERATOR::EQUAL, {value.get(), target});
-                result = evaluator.evaluate(OPERATOR::AND, {&result, &isEqual});
+                auto isEqual = EqualOperation().evaluate({value.get(), target});
+                result = OrOperation().evaluate({&result, &isEqual});
             }
             return result;
         }
@@ -359,7 +355,6 @@ private:
             throw std::runtime_error("Unsupported types for CONTAIN");
         }
         
-        Evaluator& evaluator;
         const GameEnvironment::Value* target;
     };
 
@@ -369,77 +364,8 @@ private:
     };
 
     GameEnvironment::Value evaluateImpl(std::vector<const GameEnvironment::Value*> values) const override{
-        return std::visit(ContainOperationVisitor{evaluator, values[1]}, values[0]->value);
+        return std::visit(ContainOperationVisitor{values[1]}, values[0]->value);
     }
-
-    Evaluator& evaluator;
-};
-
-// COLLECT operation supports
-// Return all elements in the list such that they are equal to given value
-// TODO -> Support Map
-class CollectOperation final : public Operation {
-public:
-    CollectOperation(Evaluator& evaluator)
-    : evaluator(evaluator)
-    {}
-private:
-    // This can be moved to Modify Operation
-    // If the given value is true, push_back the target into list
-    // If the given value is false, it does not do anything
-    struct FilterVisiter{
-        void operator()(bool value){
-            if (!value){
-                return;
-            }
-            list.push_back(std::make_unique<GameEnvironment::Value>(*target));
-        }
-
-        template <typename T>
-        void operator()([[maybe_unused]] const T& value){
-            throw std::runtime_error("Filter Visitor expects bool");
-        }
-
-        std::vector<std::unique_ptr<GameEnvironment::Value>>& list;
-        const GameEnvironment::Value* target;
-    };
-
-    struct CollectOperationVisitor {
-        CollectOperationVisitor(Evaluator& evaluator, const GameEnvironment::Value* target)
-        : evaluator(evaluator), target(target)
-        {}
-
-        GameEnvironment::Value operator()(const std::unique_ptr<GameEnvironment::List>& list){
-            auto result = std::make_unique<GameEnvironment::List>();
-
-            for (const auto& value : *list){
-                // Evaluate the given target and list are equal
-                auto isEqual = evaluator.evaluate(OPERATOR::EQUAL, {value.get(), target});
-                std::visit(FilterVisiter{*result, target}, isEqual.value);
-            }
-
-            return GameEnvironment::Value(std::move(result));
-        }
-
-        template <typename T>
-        GameEnvironment::Value operator()([[maybe_unused]] const T& value){
-            throw std::runtime_error("Unsupported types for COLLECT");
-        }
-        
-        Evaluator& evaluator;
-        const GameEnvironment::Value* target;
-    };
-
-    // COLLECT operation requires 2 arguments
-    bool getSpecificationImpl(std::vector<const GameEnvironment::Value*> values) const override {
-        return values.size() == 2;
-    };
-
-    GameEnvironment::Value evaluateImpl(std::vector<const GameEnvironment::Value*> values) const override{
-        return std::visit(CollectOperationVisitor{evaluator, values[1]}, values[0]->value);
-    }
-
-    Evaluator& evaluator;
 };
 
 // Register operation to the map
@@ -628,7 +554,7 @@ Evaluator Evaluator::defaultEvaluatorFactory(){
     evaluator.registerOperation(OPERATOR::NOT,          std::make_unique<NotOperation>());
     evaluator.registerOperation(OPERATOR::EQUAL,        std::make_unique<EqualOperation>());
     evaluator.registerOperation(OPERATOR::SIZE,         std::make_unique<SizeOperation>());
-    evaluator.registerOperation(OPERATOR::CONTAIN,         std::make_unique<ContainOperation>(evaluator));
+    evaluator.registerOperation(OPERATOR::CONTAIN,         std::make_unique<ContainOperation>());
     evaluator.registerOperation(OPERATOR::UPFROM,        std::make_unique<UpFromOperation>());
     evaluator.registerOperation(MODIFIER::REVERSE,  std::make_unique<ReverseListOperation>());
     evaluator.registerOperation(MODIFIER::SHUFFLE,  std::make_unique<ShuffleListOperation>());
